fa107dd29d483201544e9f3d83bf56c6779da5bf
Cisco <manoshalaa@gmail.com>
2025-04-04 14:08:14 +0200

    Fix: removed unnecessary comments

--- src/graphql/resolvers/area.resolvers.ts
+++ src/graphql/resolvers/area.resolvers.ts
@@ -2,12 +2,10 @@ import { AreaService } from "../../services/area.service";
 
 export const areaResolvers = {
   Query: {
-    // Fetch all areas
     getAreas: async (_, __, { prisma }) => {
       const areaService = new AreaService(prisma);
-      return areaService.getAreas();  // Correct method call
+      return areaService.getAreas();  
     },
-    // Fetch a single area by ID
     getArea: async (_, { id }, { prisma }) => {
       const areaService = new AreaService(prisma);
       return areaService.getArea(id);
@@ -20,8 +18,6 @@ export const areaResolvers = {
     },
 
     updateArea: async (_, { id, name, isActive }, { prisma }) => {
-      console.log("menna is so tired " + id); // Log the id to ensure it is passed correctly.
-    
       if (!id || isNaN(Number(id))) {
         throw new Error("Invalid area ID");
       }
